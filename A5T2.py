5adc54050124a55bbc57378444626fd2fe5a2b16
Rujula-Bhatkar <iitjee2025aim@gmail.com>
2025-06-23 23:45:18 +0530

    Add files via upload

---
+++ A5T2.py
@@ -0,0 +1,7 @@
+list=[1,2,3,4,5,6,7,8,9,10]
+print("Original list:",list)
+
+slice=list[0:5:1]
+print("Extracted first five elements:",slice)
+slice.reverse()
+print("Reversed extracted elements:",slice)
